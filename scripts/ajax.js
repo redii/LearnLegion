68a5083a89a069febeee23cc58dc508e542b0cd8
redii <redii@users.noreply.github.com>
2017-12-28 17:46:13 +0100

    v0.5

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,9 +1,31 @@
+var count = 0;
+
 function showDiv(id) {
     if(document.getElementById(id).style.display=='none') {
       document.getElementById(id).style.display='block';
     }
 }
 
+function hideUnitSelect() {
+    if(document.getElementById('mm-unit-selection').style.display=='block') {
+        document.getElementById('mm-unit-selection').style.display='none';
+      }
+}
+
 function incProgress(percentage) {
     document.getElementById('progress-bar').style.width = percentage;
+}
+
+function countChecks(id) {
+    console.log(document.getElementById(id));
+    if (count <= 3) {
+        count += 1;
+    } else {
+        console.log(document.name.checked);
+        document.getElementById(id).checked = false;
+    }
+}
+
+function test() {
+    alert(test);
 }
