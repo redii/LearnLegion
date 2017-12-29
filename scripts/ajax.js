d8b260d72d113dab6ea2622f3b25fa52cd1fff56
redii <redii@users.noreply.github.com>
2017-12-29 16:08:59 +0100

    quasi v0.9

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -6,23 +6,44 @@ function showDiv(id) {
     }
 }
 
-function hideUnitSelect() {
-    if(document.getElementById('mm-unit-selection').style.display=='block') {
-        document.getElementById('mm-unit-selection').style.display='none';
-      }
+function hideDiv(id) {
+    if(document.getElementById(id).style.display=='block') {
+        document.getElementById(id).style.display='none';
+    }
 }
 
 function incProgress(percentage) {
     document.getElementById('progress-bar').style.width = percentage;
 }
 
-function countChecks(id) {
-    console.log(document.getElementById(id));
-    if (count <= 3) {
-        count += 1;
-    } else {
-        console.log(document.name.checked);
-        document.getElementById(id).checked = false;
+// countChecks() for form2
+function countChecks(j) {
+    var total = 0;
+    for(var i=0; i < document.form2.checkb.length; i++) {
+        if(document.form2.checkb[i].checked) {
+            total = total + 1;
+        }
+        if(total > 6) {
+            document.form2.checkb[j].checked = '';
+            return false;
+        }
+    }
+    if(total == 6) {
+        showDiv('enemy-class-selection');
+    }
+}
+
+// countChecks2() for form3
+function countChecks2(j) {
+    var total = 0;
+    for(var i=0; i < document.form3.checkb.length; i++) {
+        if(document.form3.checkb[i].checked) {
+            total = total + 1;
+        }
+        if(total > 6) {
+            document.form3.checkb[j].checked = '';
+            return false;
+        }
     }
 }
 
